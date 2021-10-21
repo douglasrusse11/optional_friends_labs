a583b963894213cac7225123177ce86d1fa49cdf
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-21 12:14:53 +0100

    adds likes_to_eat, do the first test

--- src/friends.py
+++ src/friends.py
@@ -3,3 +3,8 @@ def get_name(person):
 
 def get_favourite_tv_show(person):
     return person["favourites"]["tv_show"]
+
+def likes_to_eat(person, food):
+    if food in person["favourites"]["snacks"]:
+        return True
+    return False
